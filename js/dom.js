7353cd8a84f2ffca2546b59f092bc89cefc3674c
Santiago Palacios Toro <spalaciostoro14@gmail.com>
2021-10-27 18:00:29 -0500

    se adjunta tara de dom

---
+++ js/dom.js
@@ -0,0 +1,18 @@
+//console.log(document.getElementById('tituloNombre'));
+//console.log(document.getElementById('nombre'));
+//console.log(document.getElementById('tituloApellido'));
+//console.log(document.getElementById('apellido'));
+//console.log(document.getElementById('tituloTelefono'));
+//console.log(document.getElementById('Telefono'));
+//console.log(document.getElementById('tituloCorreo'));
+//console.log(document.getElementById('correo'));
+ //console.log(document.getElementsByName("nombres"));
+ //console.log(document.getElementsByName("apellido"));
+ //console.log(document.getElementsByName("Telefono"));
+ //console.log(document.getElementsByName("correo"));
+ //console.log(document.getElementsByClassName("clase"));
+  console.log(document.getElementsByTagName('input'));
+  console.log(document.getElementsByTagName('label'));
+  console.log(document.getElementsByTagName('form'));
+  console.log(document.getElementsByTagName('script'));
+  console.log(document.getElementsByTagName('br'));
